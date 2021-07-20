9597293e249333ebc67036b9b58de3526c920a0c
QSmally <github@qbot.eu>
2021-07-20 09:29:13 +0200

    Tests: update tests

--- Test/Main.js
+++ Test/Main.js
@@ -1,8 +1,7 @@
 
 const DispatchQueue = require("../Sources/DispatchQueue");
 
-const DQ = new DispatchQueue("./Test/Thread.js", 3);
-process.env.THREAD_DEBUG = "true";
+const DQ = new DispatchQueue("./Test/Thread.js", 4);
 
 console.log(DQ);
 console.log(`[main] threads: ${DQ.threadAmount}`);
@@ -10,21 +9,43 @@ console.log(`[main] of which active: ${DQ.activeThreadAmount}`);
 
 let iteration = 0;
 
+const tests = new Map();
+
 const interval = setInterval(() => {
     iteration++;
 
     if (iteration > 100) {
         clearInterval(interval);
+        console.log("sync iterations are done");
         console.log(DQ.threadController.workers.map(W => `thread ${W.threadId}, queue size: ${W.tasks.remaining}`));
         return;
     }
 
+    tests.set(iteration, false);
+
+    if (iteration === 30) {
+        console.log("scaling to 6 threads");
+        DQ.scaleTo(6);
+    }
+
+    if (iteration === 75) {
+        console.log("scaling down to 5 threads");
+        DQ.scale(-1);
+    }
+
     DQ
-        .task({ hello: "world", iteration, shouldError: iteration === 20 })
+        .task({ hello: "world", iteration, shouldError: iteration === 40 })
         .then(result => {
             console.log(result);
+            tests.set(result.iteration, true);
+
             if (result.iteration === 100) {
-                console.log(DQ.threadController.workers.map(W => `thread ${W.threadId}, queue size: ${W.tasks.remaining}`));
+                setTimeout(() => {
+                    console.log("async tasks are completed");
+                    console.log(DQ.threadController.workers.map(W => `thread ${W.threadId}, queue size: ${W.tasks.remaining}`));
+                    console.log("expect 40 to fail, because test error causing thread exit");
+                    console.log([...tests.entries()].filter(R => R[1] === false));
+                }, 100);
             }
         })
         .catch(console.error);
--- Test/Thread.js
+++ Test/Thread.js
@@ -4,7 +4,7 @@ console.log(`thread ${threadId} spawned`);
 
 parentPort.on("message", payload => {
     setTimeout(() => {
-        if (payload.shouldError) throw new Error("[from thread] an error occurred");
+        if (payload.shouldError) throw new Error(`[from thread ${threadId}] thrown error to see a thread restart`);
         parentPort.postMessage({ ...payload, fromThread: threadId });
     }, 5).unref();
 });
