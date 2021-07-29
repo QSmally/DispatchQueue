34b84479c1c3e232d65f2886b6e37a25b94590a8
QSmally <github@qbot.eu>
2021-07-29 13:38:23 +0200

    Tests: implement `DispatchQueue.Thread`

--- Test/Thread.js
+++ Test/Thread.js
@@ -1,10 +1,17 @@
 
-const { parentPort, threadId } = require("worker_threads");
-console.log(`thread ${threadId} spawned`);
+const DispatchQueue = require("../Sources/DispatchQueue");
 
-parentPort.on("message", payload => {
-    setTimeout(() => {
-        if (payload.shouldError) throw new Error(`[from thread ${threadId}] thrown error to see a thread restart`);
-        parentPort.postMessage({ ...payload, fromThread: threadId });
-    }, 5).unref();
-});
+class Thread extends DispatchQueue.Thread {
+    onSpawn() {
+        console.log(`thread ${this.id} spawned`);
+    }
+
+    onPayload(payload) {
+        setTimeout(() => {
+            if (payload.shouldError) throw new Error(`[from thread ${this.id}] thrown error to see a thread restart`);
+            this.resolve({ ...payload, fromThread: this.id });
+        }, 5).unref();
+    }
+}
+
+new Thread();
