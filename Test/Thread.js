141c8a3e9f736dbda2f012441217ffbda3043dd1
QSmally <github@qbot.eu>
2021-08-10 14:15:49 +0200

    Tests: update `identifier` property in tests

--- Test/Thread.js
+++ Test/Thread.js
@@ -3,13 +3,13 @@ const DispatchQueue = require("../Sources/DispatchQueue");
 
 class Thread extends DispatchQueue.Thread {
     onSpawn() {
-        console.log(`thread ${this.id} spawned`);
+        console.log(`thread ${this.identifier} spawned`);
     }
 
     onPayload(payload) {
         setTimeout(() => {
-            if (payload.shouldError) throw new Error(`[from thread ${this.id}] thrown error to see a thread restart`);
-            this.resolve({ ...payload, fromThread: this.id });
+            if (payload.shouldError) throw new Error(`[from thread ${this.identifier}] thrown error to see a thread restart`);
+            this.resolve({ ...payload, fromThread: this.identifier });
         }, 5).unref();
     }
 }
