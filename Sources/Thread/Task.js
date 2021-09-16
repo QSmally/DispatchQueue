73aceba481287a4eaece5b7a1521583c36abd1ce
QSmally <github@qbot.eu>
2021-09-16 18:43:31 +0200

    Task: refactor struct

--- Sources/Thread/Task.js
+++ Sources/Thread/Task.js
@@ -5,21 +5,23 @@ class Task {
          * An outgoing payload to the thread.
          * @name Task#payload
          * @type {Object}
+         * @readonly
          */
         this.payload = payload;
-
-        /**
-         * A promise with outer `resolve` and `reject`
-         * methods for communicating with the main thread,
-         * or origin thread.
-         * @name Task#promise
-         * @type {Promise}
-         */
-        this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
-        });
     }
+
+    /**
+     * A promise with outer `resolve` and `reject`
+     * methods for communicating with the main thread,
+     * or origin thread.
+     * @name Task#promise
+     * @type {Promise}
+     * @readonly
+     */
+     promise = new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject = reject;
+    });
 }
 
 module.exports = Task;
