811523bb9ce784674e34a8ee41206d0ab2047e20
QSmally <github@qbot.eu>
2021-09-23 13:08:58 +0200

    DispatchGroup: add `dataContext` support and implement type

--- Sources/Group.js
+++ Sources/Group.js
@@ -2,15 +2,30 @@
 class DispatchGroup {
 
     /**
+     * @typedef {Object} DispatchQueueConfiguration
+     * @property {Pathlike} path A path to the thread implementation.
+     * @property {Number} threadAmount Initial amount of threads this queue
+     * should spawn. It defaults to the value returned by `os.cpus().length`.
+     * @property {Boolean} lazyInitialisation Whether or not to wait with
+     * spawning threads until the first task is registered. By default, this
+     * is disabled.
+     * @property {Object} dataContext Any data to provide to the thread.
+     */
+
+    /**
      * Initialises a group of DispatchQueue configurations.
-     * @param {Object} dispatchQueues An object with a name/properties scheme.
+     * @param {Object<String, DispatchQueueConfiguration>} dispatchQueues
+     * An object with a name/properties scheme.
      */
     constructor(dispatchQueues = {}) {
         const DispatchQueue = require("./DispatchQueue");
 
         for (const [name, properties] of Object.entries(dispatchQueues)) {
-            const { path, threadAmount, lazyInitialisation } = properties;
-            const dispatchQueue = new DispatchQueue(path, threadAmount, lazyInitialisation);
+            const { path, threadAmount, lazyInitialisation, dataContext } = properties;
+            const dispatchQueue = new DispatchQueue(path, {
+                threadAmount,
+                lazyInitialisation,
+                dataContext });
             this.threadGroups.set(name, dispatchQueue);
         }
     }
